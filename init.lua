7262806be7448032096c1216e525d79bc4a5b395
Michael Nemecky <michael@faramir.nemecky.de>
2018-02-27 22:41:19 +0100

    first working version

--- init.lua
+++ init.lua
@@ -1,45 +1,119 @@
 -- Init script Ringnet IoT
--- Based on NodeMCU documentation
+-- Based on code from
+--   NodeMCU documentation
+--   https://github.com/kovi44/NODEMCU-LUA-OTA-ESP8266
 
--- load credentials
-dofile("credentials.lua")
-
--- startup function - check for deleted init.lua
-function startup()
-    if file.open("init.lua") == nil then
-        print("init.lua deleted or renamed")
+-- load config file
+--
+function loadConfig(s)
+    if (file.open("config.ini","r")) then
+        local fileContent = file.read()
+        file.close()
+        for k, v in string.gmatch(fileContent, "([%w._]+)=([%S ]+)") do
+          s[k] = v
+        end
     else
-        print("Running")
-        file.close("init.lua")
-        dofile("main.lua")
+      print("Error (init.lua): could not load config file")
     end
 end
 
--- Wifi station event callbacks
+-- save config file
+--
+function saveConfig(s)
+    file.remove("config.ini")
+    file.open("config.ini", "w+")
+
+    for k, v in pairs(s) do
+        file.writeline(k .. "=" .. v)
+    end
+
+    file.close()
+    collectgarbage()
+end
+
+-- check for OTA update
+--
+function checkUpdate()
+    http.get("http://" .. config.host .. "/" .. config.path .. "/node.php?id=" .. chipId .. "&update", nil, function(code,data)
+        if (code==200) then
+            if string.find(data, "UPDATE")~=nil then
+                config.update="true"
+                saveConfig(config)
+                node.restart()
+            end
+        end
+    end)
+end
+
+-- startup function
+-- check for deleted init.lua, start main.lua
+function startupMain()
+
+    -- check for update request
+    if (config.update=="true") then
+        print("Info (init.lua): Update requested")
+
+        -- remove update flag to prevent boot loop
+        config.update="false"
+        saveConfig(config)
+
+        if (file.open("update.lua")) then
+            file.close()
+            dofile("update.lua")
+        else
+            print("Error (init.lua): update.lua missing")
+        end
+
+    else
+        -- set up update check, requires host config to be set
+        if (config.host ~= "") then
+            if (tonumber(config.interval) > 0) then
+                print("Info (init.lua): checking for updates with " .. config.host .. " (" .. config.interval .. " seconds)")
+                tmr.create():alarm(tonumber(config.interval)*1000, tmr.ALARM_AUTO, checkUpdate)
+            end
+        end
+
+        -- if bootfile config is set, execute it
+        if (config.bootfile~="") then
+            print("Info (init.lua): Running ",config.bootfile)
+            if (file.open(config.bootfile)) then
+                file.close()
+                dofile(config.bootfile)
+            end
+        else
+            print("Error (init.lua): No bootfile set, end of execution")
+        end
+    end
+end
+
+-- Wifi station event callback
+-- STA_CONNECTED 
 wifi_connect_event = function(T)
-    print("Connection to AP("..T.SSID..") established")
-    print("Waiting for IP address...")
+    print("Info (init.lua): Connection to " .. T.SSID .. " established, waiting for IP address...")
     if disconnect_ct ~= nil then disconnect_ct = nil end
 end
 
+-- Wifi station event callback
+-- STA_GOT_IP
 wifi_got_ip_event = function(T)
-    print("Wifi connection is ready, IP address: "..T.IP)
-    print("Startup will resume in 5 seconds.")
-    print("Waiting...")
-    tmr.create():alarm(5000,tmr.ALARM_SINGLE, startup)
+    print("Info (init.lua): IP address acquired, ",T.IP)
+    print("Info (init.lua): Startup will resume in 5 seconds...")
+    tmr.create():alarm(5000,tmr.ALARM_SINGLE, startupMain)
 end
 
+-- Wifi station event callback
+-- STA_DISCONNECTED
 wifi_disconnect_event = function(T)
     if T.reason == wifi.eventmon.reason.ASSOC_LEAVE then
       return
     end
 
-    local totalt_tries = 75
-    print("\nWifi connection to AP("..T.SSID..") has failed!")
+    local max_retries=10
+    print("Error (init.lua): Connection to " .. T.SSID .. " failed!")
 
     for key,val in pairs(wifi.eventmon.reason) do
         if val == T.reason then
-            print("Disconnect reason "..val.."("..key..")")
+            print("Error (init.lua): Disconnect reason "..val.."("..key..")")
             break
         end
     end
@@ -50,14 +124,31 @@ wifi_disconnect_event = function(T)
         disconnect_ct = disconnect_ct + 1
     end
 
-    if disconnect_ct < local_tries then
-        print("Retrying connecting to AP ("..(disconnect_ct+1).." of "..local_tries..")")
+    if disconnect_ct < max_retries then
+        print("Info (init.lua): Retrying connecting to AP, " .. (disconnect_ct+1) .. " of " .. max_retries .. " retries")
     else
         wifi.sta.disconnect()
-        print("Aborting connection to AP")
+        print("Error (init.lua): Aborting connection to AP")
         disconnect_ct = nil
     end
 end
+
+-- main routine
+chipId = node.chipid()
+print("Info (init.lua): ESP node ID " .. chipId)
+
+-- initialise config array
+--  ssid: Wifi SSID
+--  secret: Wifi password
+--  host: hostname or IP for update checks
+--  path: URL to update check
+--  bootfile: LUA file to start after initialisation
+--  interval: time interval to check for updates, default 60 seconds
+--  update: update requested
+config = { ssid="", secret="", host="", path="", err="", bootfile="", update="false", interval=60 }
+
+-- read config file
+loadConfig(config)
 
 -- register Wifi station event callbacks
 wifi.eventmon.register(wifi.eventmon.STA_CONNECTED, wifi_connect_event)
@@ -65,10 +156,10 @@ wifi.eventmon.register(wifi.eventmon.STA_GOT_IP, wifi_got_ip_event)
 wifi.eventmon.register(wifi.eventmon.STA_DISCONNECTED, wifi_disconnect_event)
 
 -- setup Wifi
-print("Connecting to AP")
+print("Info (init.lua): Connecting to AP")
 wifi_cfg={}
-wifi_cfg.ssid=WIFI_SSID
-wifi_cfg.pwd=WIFI_SECRET
+wifi_cfg.ssid=config.ssid
+wifi_cfg.pwd=config.secret
 wifi.setmode(wifi.STATION)
 wifi.sta.config(wifi_cfg)
 
---
+++ update.lua
@@ -0,0 +1,174 @@
+-- Update script Ringnet IoT
+-- Based on code from
+--   NodeMCU documentation
+--   https://github.com/kovi44/NODEMCU-LUA-OTA-ESP8266
+
+-- load config file
+--
+function loadConfig(s)
+    if (file.open("config.ini","r")) then
+        local fileContent = file.read()
+        file.close()
+        for k, v in string.gmatch(fileContent, "([%w._]+)=([%S ]+)") do
+          s[k] = v
+        end
+    else
+      print("Error (update.lua): could not load config file")
+    end
+end
+
+-- save config file
+--
+function saveConfig(s)
+    file.remove("config.ini")
+    file.open("config.ini", "w+")
+
+    for k, v in pairs(s) do
+        file.writeline(k .. "=" .. v)
+    end
+
+    file.close()
+--    collectgarbage()
+end
+
+-- split string
+--
+function splitString(str,sep)
+    if sep == nil then
+        sep = "%s"
+    end
+
+    local t={}; i=1
+    for s in string.gmatch(str, "([^"..sep.."]+)") do
+        t[i] = s
+        i=i+1
+    end
+    
+    return t
+end
+
+-- strip for header
+--
+function stripHeader(c)
+    local nStart, nEnd = string.find(c, "\n\n")
+    if (nEnd == nil) then
+        nStart, nEnd = string.find(c, "\r\n\r\n")
+    end
+    if (nEnd ~= nil) then
+        c = string.sub(c,nEnd+1)
+        return c
+    else
+        return nil
+    end
+end
+
+-- check for all files downloaded, update bootfile, restart
+--
+function endUpdate(s,c)
+    numFiles = numFiles - 1
+    if (numFiles == 0) then
+
+        sck = net.createConnection(net.TCP,0)
+
+        sck:on("receive", function(sck, data)
+            local c = stripHeader(data)
+            if (c~=nil) then
+                config.bootfile = c
+                saveConfig(config)
+                node.restart()
+            end
+        end)
+
+        sck:on("connection", function(sck)
+            sck:send("GET /" .. config.path .. "/node.php?id=" .. chipId .. "&boot HTTP/1.0\r\n" ..
+                      "Host: " .. config.host .. "\r\n" ..
+                      "Connection: close\r\n" ..
+                      "Accept-Charset: utf-8\r\n" ..
+                      "Accept-Encoding: \r\n" ..
+                      "User-Agent: Mozilla/4.0 (compatible; esp8266 Lua; Windows NT 5.1)\r\n" ..
+                      "Accept: */*\r\n\r\n")
+
+        end)
+
+        sck:connect(80,config.host)
+    end
+end
+
+-- download files to be updated
+--
+function downloadFiles(s,c)
+
+    c = stripHeader(c)
+    local f = splitString(c, "\n")
+    numFiles = table.getn(f)
+
+    for k, v in pairs(f) do
+        local buffer = nil
+        local maxlength = 1600
+        local payloadFound = nil
+
+        file.remove(v)
+
+        srv=net.createConnection(net.TCP,0)
+
+        srv:on("receive", function(sck, data)
+            if (not payloadFound) then
+                data = stripHeader(data)
+                if (data ~= nil) then
+                    payloadFound = true
+                end
+            end
+            if (payloadFound) then
+                file.open(v,"a+")
+                file.write(data)
+                file.close()
+            end
+        end)
+
+        srv:on("connection", function(sck)
+            sck:send("GET /" .. config.path .. "/uploads/" .. chipId .. "/" .. v .. " HTTP/1.0\r\n" ..
+                      "Host: " .. config.host .. "\r\n" ..
+                      "Connection: close\r\n" ..
+                      "Accept-Charset: utf-8\r\n" ..
+                      "Accept-Encoding: \r\n" ..
+                      "User-Agent: Mozilla/4.0 (compatible; esp8266 Lua; Windows NT 5.1)\r\n" ..
+                      "Accept: */*\r\n\r\n")
+        end)
+
+        srv:on("disconnection", endUpdate)
+
+        srv:connect(80,config.host)
+    end
+end
+
+
+-- main routine
+
+-- initialise config array
+--  ssid: Wifi SSID
+--  secret: Wifi password
+--  host: hostname or IP for update checks
+--  path: URL to update check
+--  bootfile: LUA file to start after initialisation
+--  interval: time interval to check for updates, default 60 seconds
+--  update: update requested
+config = { ssid="", secret="", host="", path="", err="", bootfile="", update="false", interval=60 }
+
+-- read config file
+loadConfig(config)
+
+conn = net.createConnection(net.TCP,0)
+
+conn:on("connection", function(c,p)
+    conn:send("GET /" .. config.path .. "/node.php?id=" .. chipId .. "&list" ..
+              " HTTP/1.1\r\n" ..
+              "Host: ".. config.host .. "\r\n" ..
+              "Accept: */*\r\n" ..
+              "User-Agent: Mozilla/4.0 (compatible; esp8266 Lua;)" ..
+              "\r\n\r\n") 
+end)
+
+conn:on("receive", downloadFiles)
+
+conn:connect(80,config.host)
+
